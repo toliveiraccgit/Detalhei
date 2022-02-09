1b6627ba2b5e31df5ff9f68606f93bb5a914b926
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-02-09 11:02:33 -0300

    Finalizando tela de login

---
+++ Componentes/Login/mostraSenha.js
@@ -0,0 +1,26 @@
+document.getElementById('olho').addEventListener('mousedown', function() {
+    document.getElementById('senha').type = 'text';
+    document.getElementById('senha').focus()
+    olho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus.svg") '
+  });
+  
+  document.getElementById('olho').addEventListener('mouseup', function() {
+    document.getElementById('senha').type = 'password';
+    olho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus-ativado.svg") '
+  });
+  
+  // Para que o password não fique exposto apos mover a imagem.
+  document.getElementById('olho').addEventListener('mousemove', function() {
+    document.getElementById('senha').type = 'password';
+  });
+
+  document.getElementById('olho').addEventListener('mouseleave', function() {
+    olho.style.backgroundImage = 'url("../../Assets/Login/icone-mostra-senha-login-blur.svg")'
+  });
+
+  document.getElementById('senha').addEventListener('focus', function(){
+    olho.style.backgroundImage = 'url("../../Assets/Login/botao-mostra-senha-focus-ativado.svg")'
+  })
+
+
+  let olho = document.getElementById('olho')
