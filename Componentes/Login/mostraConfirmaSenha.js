caee4cb79cb3be7acecb3b5895c8813a1dd4cf97
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-02-09 18:19:08 -0300

    Ajustando botão mostra senha

--- Componentes/Login/mostraConfirmaSenha.js
+++ Componentes/Login/mostraConfirmaSenha.js
@@ -1,34 +1,34 @@
 document.getElementById('confirma-olho').addEventListener('mousedown', function() {
     document.getElementById('confirmar-senha').type = 'text';
     document.getElementById('confirmar-senha').focus()
-    confirmaOlho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus-ativado.svg") '
+    confirmaOlho.style.backgroundImage = ` url("../../Assets/Login/botao-mostra-senha-focus-ativado.svg") `
   });
   
   document.getElementById('confirma-olho').addEventListener('mouseup', function() {
     document.getElementById('confirmar-senha').type = 'password';
-    confirmaOlho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus.svg") '
+    confirmaOlho.style.backgroundImage = ` url("../../Assets/Login/botao-mostra-senha-focus.svg") `
   });
   
   // Para que o password não fique exposto apos mover a imagem.
   document.getElementById('confirma-olho').addEventListener('mousemove', function() {
     document.getElementById('confirmar-senha').type = 'password';
-    confirmaOlho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus.svg") '
+    confirmaOlho.style.backgroundImage = ` url("../../Assets/Login/botao-mostra-senha-focus.svg") `
   });
 
 
   document.getElementById('confirmar-senha').addEventListener('mousemove', function() {
-    confirmaOlho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus.svg") '
+    confirmaOlho.style.backgroundImage = ` url("../../Assets/Login/botao-mostra-senha-focus.svg") `
   });
 
   document.getElementById('confirma-olho').addEventListener('mouseleave', function() {
-    confirmaOlho.style.backgroundImage = 'url("../../Assets/Login/icone-mostra-senha-login-blur.svg")'
+    confirmaOlho.style.backgroundImage = ` url("../../Assets/Login/icone-mostra-senha-login-blur.svg") `
   });
   document.getElementById('confirmar-senha').addEventListener('mouseleave', function() {
-    confirmaOlho.style.backgroundImage = 'url("../../Assets/Login/icone-mostra-senha-login-blur.svg")'
+    confirmaOlho.style.backgroundImage = ` url("../../Assets/Login/icone-mostra-senha-login-blur.svg") `
   });
 
   document.getElementById('confirmar-senha').addEventListener('focus', function(){
-    confirmaOlho.style.backgroundImage = 'url("../../Assets/Login/botao-mostra-senha-focus.svg")'
+    confirmaOlho.style.backgroundImage = ` url("../../Assets/Login/botao-mostra-senha-focus.svg") `
   })
 
 
