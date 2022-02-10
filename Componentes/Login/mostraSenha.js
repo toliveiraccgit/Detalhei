7809af869bc345c01ddb42f710d910c6592d318a
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-02-10 07:58:53 -0300

    Ajustando paths de botões de senha

--- Componentes/Login/mostraSenha.js
+++ Componentes/Login/mostraSenha.js
@@ -1,34 +1,34 @@
 document.getElementById('olho').addEventListener('mousedown', function() {
     document.getElementById('senha').type = 'text';
     document.getElementById('senha').focus()
-    olho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus-ativado.svg") '
+    olho.style.backgroundImage = ' url("./Assets/Login/botao-mostra-senha-focus-ativado.svg") '
   });
   
   document.getElementById('olho').addEventListener('mouseup', function() {
     document.getElementById('senha').type = 'password';
-    olho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus.svg") '
+    olho.style.backgroundImage = ' url("./Assets/Login/botao-mostra-senha-focus.svg") '
   });
   
   // Para que o password não fique exposto apos mover a imagem.
   document.getElementById('olho').addEventListener('mousemove', function() {
     document.getElementById('senha').type = 'password';
-    olho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus.svg") '
+    olho.style.backgroundImage = ' url("./Assets/Login/botao-mostra-senha-focus.svg") '
   });
 
 
   document.getElementById('senha').addEventListener('mousemove', function() {
-    olho.style.backgroundImage = ' url("../../Assets/Login/botao-mostra-senha-focus.svg") '
+    olho.style.backgroundImage = ' url("./Assets/Login/botao-mostra-senha-focus.svg") '
   });
 
   document.getElementById('olho').addEventListener('mouseleave', function() {
-    olho.style.backgroundImage = 'url("../../Assets/Login/icone-mostra-senha-login-blur.svg")'
+    olho.style.backgroundImage = 'url("./Assets/Login/icone-mostra-senha-login-blur.svg")'
   });
   document.getElementById('senha').addEventListener('mouseleave', function() {
-    olho.style.backgroundImage = 'url("../../Assets/Login/icone-mostra-senha-login-blur.svg")'
+    olho.style.backgroundImage = 'url("./Assets/Login/icone-mostra-senha-login-blur.svg")'
   });
 
   document.getElementById('senha').addEventListener('focus', function(){
-    olho.style.backgroundImage = 'url("../../Assets/Login/botao-mostra-senha-focus.svg")'
+    olho.style.backgroundImage = 'url("./Assets/Login/botao-mostra-senha-focus.svg")'
   })
 
 
